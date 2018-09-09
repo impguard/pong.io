8f4c43ebd473e95cadf6e1a3fe28aba4f7a1f1ab
kwu <kwu@riotgames.com>
2018-09-09 16:51:28 -0700

    Adds crappy frontend client

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,24 @@
-import * as socket from 'socket.io-client'
+import * as io from 'socket.io-client'
 
-const io = socket('http://localhost:7777')
+const submitElem = document.getElementById('submit')
+const hostElem = <HTMLInputElement> document.getElementById('host')
+const portElem = <HTMLInputElement> document.getElementById('port')
+const outputElem = <HTMLTextAreaElement> document.getElementById('output')
 
-io.on('time', console.log)
+const log = (data) => {
+  outputElem.value += data + '\n'
+}
+
+let socket: SocketIOClient.Socket
+
+submitElem.addEventListener('click', () => {
+  const host = hostElem.value
+  const port = portElem.value
+
+  if (socket) {
+    socket.disconnect()
+  }
+
+  socket = io(`${host}:${port}`)
+  socket.on('time', log)
+})
