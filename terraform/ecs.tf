6f99fcf01a04635222c3270f3b7e3effbcc30739
kwu <kwu@riotgames.com>
2018-09-09 18:30:45 -0700

    Modifies the ECS task role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -63,7 +63,7 @@ resource "aws_iam_role" "task" {
     {
       "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "ecs.amazonaws.com"
       },
       "Effect": "Allow",
       "Sid": ""
