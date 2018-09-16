4f7e8ee2b36f2f5f9513572ef1986a88ddaa8473
kwu <kwu@riotgames.com>
2018-09-15 21:37:35 -0700

    Added bucket and reorganized client code

--- src/client/ts/client.ts
+++ src/client/ts/client.ts
@@ -1,20 +1,9 @@
 import * as io from 'socket.io-client'
 import * as game from './game'
-
-enum Scene {
-  Home = 'home',
-  Game = 'game'
-}
+import * as scene from './scene'
 
 const state = {
-  scene: document.getElementById(Scene.Home),
   socket: null
-}
-
-const switchScene = (scene: Scene) => {
-  state.scene.style.display = 'none'
-  state.scene = document.getElementById(scene)
-  state.scene.style.display = ''
 }
 
 const play = document.getElementById('play')
@@ -31,23 +20,5 @@ play.addEventListener('click', () => {
 
   game.setup(state.socket)
 
-  switchScene(Scene.Game)
+  scene.change(scene.Name.Game)
 })
-
-// Client connects to game, gets initial game info
-// => Server can choose to block client, or not
-// => Generates physics/canvas/gamestate/all the things
-// => Physics has references to all the game state objects
-
-// It will get state from the server
-// between state updates it will...
-//
-// getinput
-// modify its player object based on input
-// run a simulation frame
-// based on location of objects, lerp position on screen
-//
-// when game state received from server
-// server reconciliation
-// => rollback current state
-// => simulate forward based on history of inputs
---
+++ src/client/ts/scene.ts
@@ -0,0 +1,12 @@
+export enum Name {
+  Home = 'home',
+  Game = 'game'
+}
+
+let scene = document.getElementById(Name.Home)
+
+export const change = (nextScene: Name) => {
+  scene.style.display = 'none'
+  scene = document.getElementById(nextScene)
+  scene.style.display = ''
+}
