d8bf95cd947671e176945980157cd86f223e33cf
kwu <kwu@riotgames.com>
2018-09-15 19:43:59 -0700

    Updates to add pixi canvas

--- src/client/ts/game.ts
+++ src/client/ts/game.ts
@@ -1,4 +1,8 @@
 import * as PIXI from 'pixi.js'
 
+const game = document.getElementById('game')
+
 export const setup = (socket: SocketIOClient.Socket) => {
+    const app = new PIXI.Application(800, 800)
+    game.appendChild(app.view)
 }
