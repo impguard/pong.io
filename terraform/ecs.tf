9ff6758e59bea022f4dfe5391898be642ac9e681
kwu <kwu@riotgames.com>
2018-09-15 18:07:41 -0700

    Updates deploy and terraform to use spot instances

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,12 +2,12 @@
 # ECR
 # --------------------------------------------------
 
-resource "aws_ecr_repository" "ecr_repository" {
+resource "aws_ecr_repository" "pong" {
   name = "${var.project_id}"
 }
 
 resource "aws_ecr_lifecycle_policy" "expiration" {
-  repository = "${aws_ecr_repository.ecr_repository.name}"
+  repository = "${aws_ecr_repository.pong.name}"
 
   policy = <<EOF
 {
@@ -50,6 +50,117 @@ resource "aws_ecs_cluster" "sandbox" {
 }
 
 # --------------------------------------------------
+# Spot Instances IAM Roles
+# --------------------------------------------------
+
+resource "aws_iam_role" "ecs-fleet" {
+  name = "pong-fleet_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "spotfleet.amazonaws.com",
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs-instance" {
+  name = "pong-instance_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "task-execution" {
+  role       = "${aws_iam_role.ecs-instance.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_instance_profile" "ecs" {
+  name = "pong-ecs_instance_profile"
+  role = "${aws_iam_role.ecs-instance.name}"
+}
+
+# --------------------------------------------------
+# Spot Fleet Request
+# --------------------------------------------------
+
+resource "aws_spot_fleet_request" "sandbox" {
+  iam_fleet_role      = "${aws_iam_role.ecs-fleet.arn}"
+  target_capacity     = "1"
+  valid_until         = "2018-09-30T00:00:00Z"
+
+  wait_for_fulfillment = true
+  timeouts {
+    create = "1m"
+  }
+
+  launch_specification {
+    instance_type            = "t2.micro"
+    ami                      = "ami-093381d21a4fc38d1"
+    iam_instance_profile_arn = "${aws_iam_instance_profile.ecs.arn}"
+    key_name                 = "pong-ecs"
+    availability_zone        = "${aws_subnet.public-subnet.availability_zone}"
+    subnet_id                = "${aws_subnet.public-subnet.id}"
+    vpc_security_group_ids   = ["${aws_security_group.public-security_group.id}"]
+    associate_public_ip_address = true
+
+    root_block_device {
+      volume_size = "10"
+      volume_type = "gp2"
+    }
+
+    ebs_block_device {
+      device_name = "/dev/xvdcz"
+      volume_size = "10"
+      volume_type = "gp2"
+    }
+
+    user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.sandbox.name} >> /etc/ecs/ecs.config
+EOF
+
+    tags {
+      Name = "pong-sandbox-fleet-ecs"
+    }
+  }
+}
+
+# --------------------------------------------------
+# Cloudwatch Group
+# --------------------------------------------------
+
+resource "aws_cloudwatch_log_group" "sandbox" {
+  name = "pong-sandbox"
+}
+
+# --------------------------------------------------
 # Task Role
 # --------------------------------------------------
 
@@ -71,22 +182,4 @@ resource "aws_iam_role" "task" {
   ]
 }
 EOF
-}
-
-
-data "aws_iam_policy" "task_execution" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "task_execution" {
-    role       = "${aws_iam_role.task.name}"
-    policy_arn = "${data.aws_iam_policy.task_execution.arn}"
-}
-
-# --------------------------------------------------
-# Cloudwatch Group
-# --------------------------------------------------
-
-resource "aws_cloudwatch_log_group" "sandbox" {
-  name = "pong-sandbox"
 }
