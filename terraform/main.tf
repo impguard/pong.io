d06c3c8fd28c66063da279a5d60d121e240dee16
kwu <kwu@riotgames.com>
2018-09-22 18:38:38 -0700

    Opens up security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,30 +98,9 @@ resource "aws_security_group" "public-security_group" {
   description = "Allow incoming HTTP connections & SSH access"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks =  ["0.0.0.0/0"]
   }
 
